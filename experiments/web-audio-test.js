e36a76c74b41f8ff0efb1c9160a6e699e6eebd25
Wes Modes <wmodes@gmail.com>
2018-03-22 23:22:12 -0700

    Mo mo mo

--- experiments/web-audio-test.js
+++ experiments/web-audio-test.js
@@ -1,9 +1,18 @@
 var AudioContext = require('web-audio-api').AudioContext;
-var Speaker      = require('speaker');
-var fs           = require('fs');
+var Speaker = require('speaker');
+var fs = require('fs');
+var rl = require('readline');
 
-var track1       = './sounds/393024__axiologus__heavy-stream-with-birds.mp3';
-var track2       = './sounds/9369__833-45__sweep01.wav';
+var prompt = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var tracks = [
+    './sounds/393024__axiologus__heavy-stream-with-birds.mp3',
+    'sounds/399744__inspectorj__ambience-florida-frogs-gathering-a.mp3',
+    './sounds/9369__833-45__sweep01.wav'
+];
 
 var context      = new AudioContext();
 
@@ -14,44 +23,80 @@ context.outStream = new Speaker({
   sampleRate: context.format.sampleRate
 });
 
+console.log("Reading files");
+var audioBuffer = Array.apply(null, Array(tracks.length)).map(function () {});
+var audioData = Array.apply(null, Array(tracks.length)).map(function () {});
+for (i=tracks.length-1; i>=0; i--) {
+    console.log("track", i, ":", tracks[i]);
+    audioData[i] = fs.readFileSync(tracks[i]);
+    console.log("Decode track", i);
+    (function(index) {
+        context.decodeAudioData(audioData[index], function(newBuffer) {
+            console.log("Decode track", index, "- done");
+            audioBuffer[index] = newBuffer;
+            if (audioBuffer.every(x => x)) {
+                playAll();
+            }
+        });
+    })(i);
+}
+
 function play(audioBuffer) {
   if (!audioBuffer) { return; }
-
   var bufferSource = context.createBufferSource();
-
   bufferSource.connect(context.destination);
   bufferSource.buffer = audioBuffer;
   bufferSource.loop   = false;
   bufferSource.start(0);
 }
 
-console.log("Reading files");
-var audioData1 = fs.readFileSync(track1);
-var audioData2 = fs.readFileSync(track2);
+function playloop(audioBuffer) {
+  if (!audioBuffer) { return; }
+  var bufferSource = context.createBufferSource();
+  bufferSource.connect(context.destination);
+  bufferSource.buffer = audioBuffer;
+  bufferSource.loop   = true;
+  bufferSource.start(0);
+}
 
-var audioBuffer1, audioBuffer2;
+function pause(audioBuffer) {
+  if (!audioBuffer) { return; }
+  var bufferSource = context.createBufferSource();
+  bufferSource.connect(context.destination);
+  bufferSource.buffer = audioBuffer;
+  bufferSource.stop(0);
+}
 
-console.log("Decode track1");
-context.decodeAudioData(audioData1, function(audioBuffer) {
-  console.log("Decode track1 - done");
-  audioBuffer1 = audioBuffer;
-  if (audioBuffer1 && audioBuffer2) { playBoth(); }
-});
+function playAll() {
 
-console.log("Decode track2");
-context.decodeAudioData(audioData2, function(audioBuffer) {
-  console.log("Decode track2 - done");
-  audioBuffer2 = audioBuffer;
-  if (audioBuffer1 && audioBuffer2) { playBoth(); }
-});
+    var playing = next = 0;
+    var exit = false;
 
-function playBoth() {
-  console.log('playing track 1...');
-  play(audioBuffer1);
+    while (! exit) {
+        play(audioBuffer[next]);
+        playing = next;
+        //next = parseInt(readline("Next file (0-"+(tracks.length-2)+")? "));
+		prompt.question("Next file (0-"+(tracks.length-2)+")? ", function(answer) {
+			next = parseInt(answer);
+        });
+        if (next == -1) {
+            exit = true;
+        }
+        play(audioBuffer[tracks.length-1]);
+        pause(audioBuffer[playing]);
+    }
 
-  console.log('5 second delay');
-    setTimeout(function () {
-        console.log('playing track 2...');
-        play(audioBuffer2);
-    }, 5000);
+//    console.log('playing track 1...');
+//    play(audioBuffer[0]);
+//
+//    setTimeout(function () {
+//        console.log('playing track 3...');
+//        play(audioBuffer[2]);
+//    }, 5000);
+//
+//    setTimeout(function () {
+//        console.log('playing track 2...');
+//        play(audioBuffer[1]);
+//    }, 6000);
+
 }
