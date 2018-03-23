5655d1592a858a4582e414a9b048d0143469798c
Wes Modes <wmodes@gmail.com>
2018-03-22 17:03:41 -0700

    Mod test

--- experiments/aplay-test.js
+++ experiments/aplay-test.js
@@ -1,19 +1,21 @@
 var Sound = require('node-aplay');
 
 // fire and forget:
-new Sound('./sounds/393024__axiologus__heavy-stream-with-birds.wav').play();
+//new Sound('./sounds/393024__axiologus__heavy-stream-with-birds.wav').play();
 
 // with ability to pause/resume:
-var music = new Sound('/path/to/the/file/filename.wav');
+var music = new Sound('./sounds/393024__axiologus__heavy-stream-with-birds.wav');
 music.play();
 
 setTimeout(function () {
 	music.pause(); // pause the music after five seconds
+    console.log('Paused');
 }, 5000);
 
 setTimeout(function () {
 	music.resume(); // and resume it two seconds after pausing
-}, 7000);
+    console.log('Resumed');
+}, 8000);
 
 // you can also listen for various callbacks:
 music.on('complete', function () {
